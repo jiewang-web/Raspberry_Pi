61dc3fe385cf30e77ad4fbd06137a3404aff8726
wjcqmz520 <54387540+wjcqmz520@users.noreply.github.com>
2019-12-09 04:38:33 -1000

    socket_client

--- wireless_connection/client.py
+++ wireless_connection/client.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+#!/usr/bin/python2.7
 import socket
 import time
 import ultra_fun
 from DHT22_fun import DHT
 
-TCP_IP = '192.168.1.17'#'127.27.180.36'#
+TCP_IP = '172.27.15.20'#'192.168.1.17'#'127.27.180.36'#
 TCP_PORT = 6677
 BUFFER_SIZE = 1024
 # MESSAGE: ultrasonic_sensor
@@ -19,7 +20,7 @@ MESSAGE2 = DHT()
 temper = MESSAGE2[0]
 humidity = MESSAGE2[1]
 
-MESSAGE = (str(MESSAGE0) + '\n' + str(humidity) + '\n' + str(temper)) #+ str(MESSAGE2) #
+MESSAGE = (str(MESSAGE0) + ',' + str(humidity) + ',' + str(temper)) #+ str(MESSAGE2) #
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
